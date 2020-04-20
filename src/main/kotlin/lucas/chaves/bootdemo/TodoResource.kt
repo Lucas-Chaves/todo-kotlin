485954e4e94618b73c489ab50250dc1690d10a70
Lucas Chaves <lucas.chaves@kipstone.com.br>
2020-04-20 16:27:08 -0300

    all function on to do and config with bd

--- src/main/kotlin/lucas/chaves/bootdemo/TodoResource.kt
+++ src/main/kotlin/lucas/chaves/bootdemo/TodoResource.kt
@@ -3,6 +3,7 @@ package lucas.chaves.bootdemo
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import java.lang.Exception
 import java.time.Instant
 import javax.persistence.*
 
@@ -10,16 +11,33 @@ import javax.persistence.*
 interface TodoRepository : JpaRepository<Todo, Long>
 
 @RestController
-@RequestMapping(value = "/todo")
+@RequestMapping(value = ["/todo"])
 @EnableWebMvc
 class TodoResource(val todoRepo: TodoRepository) {
 
-    @GetMapping(value = "/")
-    fun getAll() {
-        todoRepo.findAll()
+    @GetMapping(value = ["/"])
+    fun getAll() = todoRepo.findAll()
+
+    @GetMapping(value = ["/{id}"])
+    fun getOne(@PathVariable id: Long) = todoRepo.findById(id)
+
+    @PostMapping(value = ["/"])
+    fun new(@RequestBody text: String) = todoRepo.save(Todo(text = text))
+
+    @DeleteMapping(value = ["/{id}"])
+    fun delete(@PathVariable id: Long) = todoRepo.deleteById(id)
+
+    @PutMapping(value = ["/{id}"])
+    fun update(@PathVariable id: Long, @RequestBody todo: Todo): Todo {
+        val toUpdate: Todo = todoRepo.findById(id).orElseThrow { Exception("Server error") }
+        toUpdate.text = todo.text
+        toUpdate.done = todo.done
+        return todoRepo.save(toUpdate)
     }
 }
 
 @Entity
-class Todo(@Id @GeneratedValue(strategy = GenerationType.AUTO)
-           val Id: Long = 0, var text: String = "", var done: Boolean = false, val createdAt: Instant = Instant.now())
+class Todo(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        val Id: Long = 0, var text: String = "", var done: Boolean = false, val createdAt: Instant = Instant.now())
